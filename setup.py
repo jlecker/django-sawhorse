eb4e287d1043574bbd3f81dd90b7022ff8c302ff
James Lecker Jr <james@jameslecker.com>
2012-03-10 01:16:33 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+import sawhorse
+
+
+setup(
+    name='django-sawhorse',
+    version=sawhorse.__version__,
+    author='James Lecker Jr',
+    author_email='james@jameslecker.com',
+    url='https://github.com/jlecker/django-sawhorse',
+    description='An installable Django project for building and testing individual apps.',
+    packages=['sawhorse'],
+    scripts=['shmanage.py'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
