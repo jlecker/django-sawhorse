b1ae5be45be110b4f721a63ce383399f31d55f89
James Lecker Jr <james@jameslecker.com>
2012-03-10 19:20:17 -0600

    check for existence of SAWHORSE_HOME and create subdirectory for app if needed

--- shmanage.py
+++ shmanage.py
@@ -4,22 +4,28 @@ import os, sys
 
 if __name__ == '__main__':
     
-    if not os.environ.get('SAWHORSE_HOME'):
+    sawhorse_home = os.environ.get('SAWHORSE_HOME')
+    if not sawhorse_home:
         raise Exception('Required environment variable SAWHORSE_HOME not found.')
+    if not os.path.exists(sawhorse_home):
+        raise Exception('Path specified in SAWHORSE_HOME does not exist. Aborting.')
     
-    wh = os.environ.get('WORKON_HOME')
-    if not wh:
+    workon_home = os.environ.get('WORKON_HOME')
+    if not workon_home:
         raise Exception('WORKON_HOME not found in environment. Sawhorse requires virtualenvwrapper.')
     
-    ve = os.environ.get('VIRTUAL_ENV')
-    if not ve:
+    venv = os.environ.get('VIRTUAL_ENV')
+    if not venv:
         raise Exception('No virtualenv activated.')
 
-    if ve.find(wh):
+    if venv.find(workon_home):
         raise Exception('Active virtualenv not in WORKON_HOME. Aborting.')
 
     # if SAWHORSE_APP is not set, attempt to guess from virtualenv name
-    os.environ.setdefault('SAWHORSE_APP', ve[len(wh) + 1:])
+    app_name = os.environ.setdefault('SAWHORSE_APP', venv[len(workon_home) + 1:])
+    app_root = os.path.join(sawhorse_home, app_name)
+    if not os.path.exists(app_root):
+        os.mkdir(app_root, 0755)
     
     os.environ['DJANGO_SETTINGS_MODULE'] = 'sawhorse.settings'
 
