7c2df8562b7aff18dd7ba6687afe4ba61278c2a7
James Lecker Jr <james@jameslecker.com>
2012-05-04 23:50:14 -0500

    automatically find and load urlconfs for installed apps

--- sawhorse/urls.py
+++ sawhorse/urls.py
@@ -5,8 +5,21 @@ from django.contrib import admin
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
-#    url('^%s/' % settings.APP_NAME, include('%s.urls' % settings.APP_NAME)),
+app_urls = []
+for app_name in settings.INSTALLED_APPS:
+    if app_name.startswith('django.contrib'):
+        # don't include contrib apps
+        # admin apps are included explicitly below
+        continue
+    app_label = app_name.rsplit('.', 1)[-1]
+    try:
+        app_urls.append(url('^%s/' % app_label, include('%s.urls' % app_name)))
+    except ImportError:
+        pass
+
+app_urls.extend([
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+])
+
+urlpatterns = patterns('', *app_urls)
